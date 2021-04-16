a2fc6b511df823feac039880d3e060a33d2593a1
Hazem Khaled <hazem.khaled@gmail.com>
2021-04-16 19:24:13 +0300

    Finally login success

--- client.js
+++ client.js
@@ -1,22 +1,33 @@
-Meteor.startup(() => Meteor.subscribe('auth.user.info'));
+import firebase from "firebase/app";
 
-Accounts.registerClientLoginFunction('firebase', loginWithFirebase);
+import "firebase/auth";
 
-Meteor.loginWithFirebase = (...args) => Accounts.applyLoginFunction('firebase', args);
+Accounts.registerClientLoginFunction('firebase', (token) => {
+  Accounts.callLoginMethod({
+    methodArguments: [{ token }]
+  });
+});
 
-/**
- * 
- * @param {*} callback 
- */
-async function loginWithFirebase() {
+Meteor.startup(() => {
+  // FIXME: Move this part to init function to get config from parent app
+  firebase.initializeApp({
+    apiKey: "AIzaSyBiTCTDP8aJt3EIrkrpAN6edFi0hu4aKVI",
+    authDomain: "knawat-auth-dev.firebaseapp.com",
+    projectId: "knawat-auth-dev",
+    storageBucket: "knawat-auth-dev.appspot.com",
+    messagingSenderId: "191490571662",
+    appId: "1:191490571662:web:7a42f9b509f23cc5f778df"
+  });
+
   firebase.auth().onAuthStateChanged(((user) => {
     if (user && Meteor.loggingIn() === false) {
-        user.getToken().then(((token) => {
-            Accounts.callLoginMethod({ methodArguments: [{ firebaseToken: token }] });
-        }))
+      user.getIdToken().then(((token) => {
+
+        Accounts.callLoginFunction('firebase', token);
+      }))
     } else if (!user) {
-        Meteor.logout()
-        // TODO: redirect to accounts.knawat.com with redirect query params to current page
+      Meteor.logout()
+      //TODO: redirect to login page
     }
   }));
-};
+})
--- server.js
+++ server.js
@@ -1,52 +1,9 @@
-import FirebaseAuth from './firebase-auth.js';
+import { check } from 'meteor/check'
 
-Meteor.startup(() => new FirebaseAuth());
+Accounts.registerLoginHandler('firebase', ({ token }) => {
+  check(token, String);
 
-const publishFields = [
-  'services',
-];
+  //TODO: Validate the token from firebase admin
 
-Meteor.publish('auth.user.info', () =>
-  Meteor.users.find(Meteor.userId(), {
-    fields: publishFields.reduce((acc, crr) => {
-      acc[crr] = 1;
-      return acc;
-    }, {}),
-  })
-);
-
-Accounts.registerLoginHandler('firebase', firebaseLoginHandler);
-
-Accounts.onLogin(res => {	
-  const user = res.user;
-  const firebaseToken = user?.services?.firebase?.token;
-
-  if (!firebaseToken) return;
-
-  updateAppUser(user.id, firebaseToken);
-  // TODO: Fetch all user data from token
-})
-
-/**
- * Update the data in the app database
- * @param {String} token
- */
-export function updateAppUser(token) {
-  const sanitizedUser = {
-    'services.firebase.token': token,
-  };
-  Meteor.users.update(id, { $set: sanitizedUser });
-}
-
-/**
- * Function to login the user
- * @param {*} options 
- */
-function firebaseLoginHandler(userToken) {
-  if (!userToken) throw new Error('Can not login. No token.');
-
-  const user = Accounts.updateOrCreateUserFromExternalService('firebase', { id: userToken });
-  updateAppUser(user.userId, userToken);
-
-  return user;
-}
+  return Accounts.updateOrCreateUserFromExternalService('knawat', { id: token });
+});

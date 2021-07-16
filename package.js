52389555f76b455e7abd7b48985c30f49d9774f0
Hazem Khaled <hazem.khaled@gmail.com>
2021-07-16 15:29:24 +0300

    New version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'knawat:accounts-firebase',
-  version: '1.0.6',
+  version: '1.0.7',
   summary: 'Login service via Firebase Authentication',
   git: 'https://github.com/Knawat/meteor-accounts-firebase.git',
   documentation: 'README.md'
